b85f6ebd019a09d3cc3126bcc5f1468fc1954aa8
Nikola Vushkov <bateshot@gmail.com>
2016-02-12 15:54:58 +0200

    Basic interface working
    TO DO:
    - resp des
    - stats
    - ranking

--- public/js/game/game-controller.js
+++ public/js/game/game-controller.js
@@ -1,15 +1,58 @@
 (function () {
     'use strict';
 
-    function GameController($location, $route, $routeParams, gameService, statistics) {
+    function GameController($scope, $location, $route, $routeParams, gameService, statistics) {
         var vm = this;
         vm.progress = [];
         vm.currentGame = {};
+        vm.time = undefined;
+        vm.keyboard = [];
+        vm.code = '';
+        vm.submit = {
+            disabled: true
+        }
 
         gameService.newGame($routeParams.type)
             .then(function(data){
                 vm.data = data;
+                initiateKeyboard($routeParams.type);
             });
+
+
+        function initiateKeyboard(type){
+            var count = 10,
+                i = 0,
+                btn = {},
+                extraDigits = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+            if(type == 2 || type == 3){
+                count = 16
+            }
+            for(i =  0; i < count; i += 1){
+                btn = {};
+                btn.disabled = false;
+                if(i < 10){
+                    btn.value = i;
+                } else {
+                    btn.value = extraDigits[i - 10];
+                }
+                vm.keyboard.push(btn);
+            }
+        }
+
+        //Relative Timer
+        function startTimer(){
+            vm.time = 0;
+            setInterval(function(){
+                if(vm.time !== undefined){
+                    $scope.$apply(function(){
+                        vm.time += 1;
+                    })
+                } else{
+                    return;
+                }
+            }, 1000)
+        }
 
 
         vm.newGame = function newGame(type){
@@ -18,14 +61,21 @@
             } else{
                 $location.path('/game/' + type);
             }
+
         }
 
         vm.guess = function(code){
+            if(!vm.time){
+                startTimer();
+            }
+            vm.clear();
+            vm.submit.disabled = true;
             gameService.guessCode(code)
                 .then(function(res){
                     //Win condition
                     if(res.moveCount){
                         console.log('VICTORY');
+                        vm.time = undefined;
                         var gameParams = res.moveCount + '/' +
                                          res.gameTime + '/' +
                                          vm.data.type
@@ -36,6 +86,42 @@
                         result: 'Bx' + res.bulls + ' Cx' + res.cows
                     });
                 })
+        }
+
+
+        //register code input
+        vm.input = function input(val){
+            var codeLength = 4;
+            if($routeParams.type == 0){
+                codeLength = 3;
+            }
+            if($routeParams.type == 3){
+                codeLength = 5;
+            }
+
+            vm.keyboard.map(function(key){
+                if(key.value == val){
+                    key.disabled = true;
+                    vm.code += val;
+                    return
+                }
+            })
+            // input validation
+            if(vm.code.length == codeLength){
+                vm.submit.disabled = false;
+                vm.keyboard.map(function(key){
+                    key.disabled = true;
+                })
+            }
+        }
+
+        //resets the keyboard and clears current code
+        vm.clear = function clear(){
+            vm.code = '';
+            vm.submit.disabled = true;
+            vm.keyboard.map(function(key){
+                key.disabled = false;
+            })
         }
 
         statistics.getStats($routeParams.type)
@@ -49,5 +135,5 @@
     }
 
     angular.module('myApp.controllers')
-        .controller('GameController', ['$location', '$route', '$routeParams', 'gameService', 'statistics', GameController])
+        .controller('GameController', ['$scope', '$location', '$route', '$routeParams', 'gameService', 'statistics', GameController])
 }());
